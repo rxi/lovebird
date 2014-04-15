4ae1dcd5a58a0f543647728e11ebe7d8c6f8377b
rxi <rxi@users.noreply.github.com>
2014-04-15 21:23:21 +0100

    Version 0.2.0
    Since version 0.1.0:
    
    * Adds support for tables with mixed keys
    * Adds support for numerical table keys
    * Adds support for up/down keys to recall input history
    * Adds counter next to repeated printed lines
    * Pre-compiles page templates
    * Fixes printed new lines
    * Improves error message clarity
    * Lots of internal changes and fixes

--- lovebird.lua
+++ lovebird.lua
@@ -9,7 +9,7 @@
 
 local socket = require "socket"
 
-local lovebird = { _version = "0.1.0" }
+local lovebird = { _version = "0.2.0" }
 
 lovebird.loadstring = loadstring or load
 lovebird.inited = false
