e84abe7b56a65ccb3ec6288e1955b6d772d41431
rxi <rxi@users.noreply.github.com>
2017-05-06 08:28:00 +0100

    Version 0.4.3

--- lovebird.lua
+++ lovebird.lua
@@ -9,7 +9,7 @@
 
 local socket = require "socket"
 
-local lovebird = { _version = "0.4.2" }
+local lovebird = { _version = "0.4.3" }
 
 lovebird.loadstring = loadstring or load
 lovebird.inited = false
